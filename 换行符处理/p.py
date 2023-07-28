def113ca65d8dea7c794b1fb699018f6b6f1d91d
Z50c <139544054+Z50c@users.noreply.github.com>
2023-07-28 12:59:26 +0800

    Update p.py

--- 换行符处理/p.py
+++ 换行符处理/p.py
@@ -1,19 +1,17 @@
 from pathlib import Path
-
 p=list(Path(".").glob("*"))
-
+a=[str(x) for x in Path(".").iterdir()if x.is_dir()]
+i=0
+while i!=len(a):
+    a=a+[str(x) for x in Path(a[i]).iterdir()if x.is_dir()]
+    p=p+list(Path(a[i]).glob("*"))
+    i=i+1
+del a
 for i in range(len(p)):
-
     try:
-
-        with open(str(p),"r")as a:
-
+        with open(str(p[i]),"r")as a:
             a=a.read()
-
-        with open(str(p),"w")as b:
-
+        with open(str(p[i]),"w")as b:
             b.write(a)
-
     except OSError:
-
         pass
