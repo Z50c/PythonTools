ca346f2ef3ac58ded1db0399cd1130431bc580e6
Z50c <139544054+Z50c@users.noreply.github.com>
2023-07-28 13:36:47 +0800

    Update p.py

--- 换行符处理/p.py
+++ 换行符处理/p.py
@@ -1,17 +1,33 @@
-from pathlib import Path
-p=list(Path(".").glob("*"))
-a=[str(x) for x in Path(".").iterdir()if x.is_dir()]
-i=0
-while i!=len(a):
-    a=a+[str(x) for x in Path(a[i]).iterdir()if x.is_dir()]
-    p=p+list(Path(a[i]).glob("*"))
-    i=i+1
-del a
-for i in range(len(p)):
-    try:
-        with open(str(p[i]),"r")as a:
-            a=a.read()
-        with open(str(p[i]),"w")as b:
-            b.write(a)
-    except OSError:
-        pass
+try:
+    from pathlib import Path
+    p=list(Path(".").glob("*"))
+    a=[str(x) for x in Path(".").iterdir()if x.is_dir()]
+    i=0
+    while i!=len(a):
+        try:
+            a=a+[str(x) for x in Path(a[i]).iterdir()if x.is_dir()]
+            p=p+list(Path(a[i]).glob("*"))
+            i=i+1
+        except PermissionError:
+            print("文件"+a[i]+"权限不够")
+            del a[i]
+    del a,i
+    d=0
+    for i in range(len(p)):
+        try:
+            with open(str(p[i]),"r")as a:
+                a=a.read()
+            with open(str(p[i]),"w")as b:
+                b.write(a)
+            d=d+1
+        except OSError:
+            pass
+        except PermissionError:
+            print("文件"+p[i]+"权限不够")
+        except UnicodeDecodeError:
+            print("文件"+str(p[i])+"编码错误")
+        finally:
+            pass
+    print("处理了"+str(d)+"个文件")
+except PermissionError:
+    print("权限不够!")
